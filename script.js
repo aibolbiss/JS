0299896f88964a15eaf7e1c3be1e45a6f6ccad16
zhabchuk <78416642+zhabchuk@users.noreply.github.com>
2021-04-21 18:54:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Вопросы 1 и 2
+const firstName = 'Sergey' // string
+console.log(typeof firstName)
+var age = 30 // number
+console.log(typeof age)
+var myAge30 = true // boolean
+console.log(typeof myAge30)
+var userName = // undefined
+console.log(typeof userName)
+var isNull = null // null
+console.log(typeof isNull)
+var userInfo = {firstName: 'Sergey', age: 30} // object
+console.log(userInfo)
+
+// Вопрос 3
+// Првильные имена переменных
+var firstName
+let firstName1
+const _
+var $
+var _name
+var $name
+
+// Непарвильные имена переменных
+запрещено использовать зарезервированные имена
+var 1
+var 1Nan
+var my-name
+
+// Вопрос 4
+let world = 'Hello world'
+console.log(world)
+
+
+// Вопрос 5
+5==2 && 4>2 - false
+
+// Вопрос 6
+5==5 || 5>9 - true
+
+// Вопрос 7
+5>2 && 7>12 || 7>3 && 6<8 - true
+
+// Вопрос 8
+if(7>3){ 
+    alert('Hello everybody')
+} - 'Hello everybody'
+
+// Вопрос 9
+if(5>8 && 2>1){ 
+    alert('Hello')
+}else{
+  alert('Bye Bye')
+} - 'Bye Bye'
+
+// Вопрос 10
+8%3 - 2
